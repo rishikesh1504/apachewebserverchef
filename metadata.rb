db27b024d7c563a680ba28c70d442096b514d128
Ubuntu <azureuser@chef-vm.0hsluijbgsretiu1jxw520w0zf.rx.internal.cloudapp.net>
2021-12-09 09:52:02 +0000

    metadatfile changed

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
-name 'apachewebser'
-maintainer 'The Authors'
+name 'apachewebserverchef'
+'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-description 'Installs/Configures apachewebser'
-long_description 'Installs/Configures apachewebser'
+description 'Installs/Configures apachewebserverchef'
+long_description 'Installs/Configures apachewebserverchef'
 version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/apachewebser/issues' if respond_to?(:issues_url)
+# issues_url 'https://github.com/<insert_org_here>/apachewebserverchef/issues' if respond_to?(:issues_url)
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/apachewebser' if respond_to?(:source_url)
+# source_url 'https://github.com/<insert_org_here>/apachewebserverchef' if respond_to?(:source_url)
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,8 +1,8 @@
 package 'apache2' do
   action :install
 end
-file "/var/www/html/index.html" do
-  content "<h1> hello chef </h1>"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 service "apache2" do
   action [ :enable,:start]
