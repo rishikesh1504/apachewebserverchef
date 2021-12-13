99f1e0fe7190c4c1827bb5b10b5ca98f34b6aae9
Ubuntu <azureuser@chef-vm.0hsluijbgsretiu1jxw520w0zf.rx.internal.cloudapp.net>
2021-12-13 11:08:52 +0000

    server.rb and index.html modified

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,10 +1,13 @@
 package 'apache2' do
   action :install
 end
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  variables( :name => 'rishikesh')
+cookbook_file '/var/www/html/index.html' do 
+    source 'index.html'
 end
+#template '/var/www/html/index.html' do
+ # source 'index.html.erb'
+ # variables( :name => 'rishikesh')
+#end
 service "apache2" do
   action [ :enable,:start]
 end
